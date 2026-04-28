3f7b26016703c6c372ac6282922f6226c83212b1
819462 <154494294+819462@users.noreply.github.com>
2026-04-28 14:18:12 -0700

    Create exec_error.java

---
+++ backend/srvr_1/exec_error.java
@@ -0,0 +1,8 @@
+/*
+ * This first runs the code allocated to it.
+ * If this code returns errors (file access, external classes, etc), call "change.java" to fix it.
+ */
+
+public class exec_error {
+  
+}
